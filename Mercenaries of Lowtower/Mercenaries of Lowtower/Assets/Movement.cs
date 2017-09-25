329c5d320383d3636bb6d31e655c2f7af9314119
Benjamin McEachrane <benjamin.mceachrane@gmail.com>
2017-09-25 18:19:27 -0400

    Apply First Testing Commit
    Folder structure created for Benjamin's testing scene. Capsule with character
    controller in main scene. Basic movement coded. Character cannot adjust
    momentum mid-air.

---
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    public float speed;
+    public float jumpSpeed;
+    public float gravity;
+    private Vector3 moveDirection = Vector3.zero;
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+
+        //can move midair but gravity is hella weak
+        
+
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+
+            if (Input.GetButton("Jump") && controller.isGrounded)
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
