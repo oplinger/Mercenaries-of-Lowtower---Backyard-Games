c4ea8f0d38c3984f1f846387a79d94ce0011256c
Travis Willis <willtrav@sheridancollege.ca>
2017-09-25 12:40:22 -0400

    Add Boss Targeting Script Beta

---
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Scripts/BossTargetingAI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossTargetingAI : MonoBehaviour {
+
+   public Collider[] targets;
+    public float[] targetThreat;
+
+	// Use this for initialization
+	void Start () {
+        FindTarget();
+        targetThreat = new float[targets.Length];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    void FindTarget()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, 100, 1 << 8);
+        //print(LayerMask.NameToLayer("Players"));
+        targets = hitColliders;
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, 100);
+    }
+}
