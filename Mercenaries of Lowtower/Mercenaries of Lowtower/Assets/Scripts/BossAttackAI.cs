4f48b961a96f8607d425a79425569b3c6a9a05fd
Travis Willis <willtrav@sheridancollege.ca>
2017-09-26 13:42:18 -0400

    Add Global CoolDown (GCD), and option for individual cooldowns(for animations, or cinematic attacks). Expand attack priority system.

--- Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Scripts/BossAttackAI.cs
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Scripts/BossAttackAI.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class BossAttackAI : MonoBehaviour {
     public float[] attackCDs;
     BossTargetingAI targeting;
+   public float currentGCD;
+    float maxGCD;
 	// Use this for initialization
 	void Start () {
         attackCDs = new float[4];
@@ -18,32 +20,19 @@ public class BossAttackAI : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+        currentGCD += Time.deltaTime;
         attackCDs[0] -= Time.deltaTime;
         attackCDs[1] -= Time.deltaTime;
         attackCDs[2] -= Time.deltaTime;
         attackCDs[3] -= Time.deltaTime;
 
-
-        for (int i = 0; i < 4;)
+        if (currentGCD >= maxGCD)
         {
-            if (attackCDs[0] <= 0)
-            {
-                Attack1(targeting.currentTarget);
-            }
-            if (attackCDs[1] <= 0)
-            {
-                Attack1(targeting.currentTarget);
-            }
-            if (attackCDs[2] <= 0)
-            {
-                Attack1(targeting.currentTarget);
-            }
-            if (attackCDs[3] <= 0)
-            {
-                Attack1(targeting.currentTarget);
-            }
+            checkAttacks();
+            
         }
+        
+
 
     }
 
@@ -52,6 +41,7 @@ public class BossAttackAI : MonoBehaviour {
         Health health = target.GetComponent<Health>();
         health.modifyHealth(20, 9);
         attackCDs[0] = 10;
+        triggerGCD(2);
         print("ATTACK1 USED");
     }
 
@@ -60,6 +50,7 @@ public class BossAttackAI : MonoBehaviour {
         Health health = target.GetComponent<Health>();
         health.modifyHealth(10, 9);
         attackCDs[1] = 8;
+        triggerGCD(2);
         print("ATTACK2 USED");
     }
 
@@ -68,6 +59,7 @@ public class BossAttackAI : MonoBehaviour {
         Health health = target.GetComponent<Health>();
         health.modifyHealth(5, 9);
         attackCDs[2] = 5;
+        triggerGCD(2);
         print("ATTACK3 USED");
     }
     
@@ -76,7 +68,34 @@ public class BossAttackAI : MonoBehaviour {
         Health health = target.GetComponent<Health>();
         health.modifyHealth(1, 9);
         attackCDs[3] = 2;
+        triggerGCD(2);
         print("ATTACK4 USED");
+    }
+
+    void checkAttacks()
+    {
+        if (attackCDs[0] <= 0)
+        {
+            Attack1(targeting.currentTarget);
+            
+        } else if (attackCDs[1] <= 0)
+        {
+            Attack2(targeting.currentTarget);
+        }
+         else if (attackCDs[2] <= 0)
+        {
+            Attack3(targeting.currentTarget);
+        }
+         else if (attackCDs[3] <= 0)
+        {
+            Attack4(targeting.currentTarget);
+        }
+    }
+
+    void triggerGCD(float cooldownTime)
+    {
+        currentGCD = 0;
+        maxGCD = cooldownTime;
     }
 }
 
