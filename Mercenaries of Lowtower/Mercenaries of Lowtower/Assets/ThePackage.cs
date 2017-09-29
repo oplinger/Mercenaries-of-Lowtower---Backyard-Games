02f256b85bccb27a4a6ac0316bae3f4b0d5321de
Benjamin McEachrane <benjamin.mceachrane@gmail.com>
2017-09-29 12:18:48 -0400

    Add AI Functionality
    Package moves forward on its own. Enemy moves toward package.
    Package turns red if touched by an enemy.

---
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/ThePackage.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThePackage : MonoBehaviour {
+
+    public float speed;
+    Renderer rend;
+
+    // Use this for initialization
+    void Start () {
+
+        rend = GetComponent<Renderer>();
+        rend.material.shader = Shader.Find("Specular");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += Vector3.forward*speed*Time.deltaTime;
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="Enemy")
+        {
+            rend.material.color = Color.red;
+            int timer = 30;
+            timer--;
+
+            if (timer<=0)
+            {
+                rend.material.color = Color.white;
+
+            }
+        }
+    }
+}
