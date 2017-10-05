72a8167a404ae3cef6fbda2bb37d0c0f75d6715c
SHERNET\WINGEJES <wingejes@sheridancollege.ca>
2017-10-04 20:44:40 -0400

    Door Implemtations
    Implemented Switches for the Door
    > Standing on all 4 different coloured switches will open the door enough for the player to go through
    > When the player collides with the coloured panels the door moves up 1 position
    > This will eventually allow the different players to trigger 4 different unique "locks"

---
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Scripts/Door Scripts/YellowTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YellowTrigger : MonoBehaviour
+{
+
+    public GameObject door;
+    bool trigger = true;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && trigger == true)
+        {
+            door.transform.position += new Vector3(0, 1, 0);
+            trigger = false;
+            //door moves up
+        }
+        }
+    }
+
