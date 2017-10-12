f5a3ca7d5af3f87027845ec0c00437f2850be693
Travis Willis <willtrav@sheridancollege.ca>
2017-10-12 16:04:53 -0400

    Add UI functionality for Health Bars and Cooldowns Fix minor bug fixes

--- Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Scripts/UIController.cs
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Scripts/UIController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIController : MonoBehaviour {
+public class UIController : MonoBehaviour
+{
     public GameObject[] players;
     public GameObject boss;
     public GameObject controllerThing;
@@ -25,7 +26,8 @@ public class UIController : MonoBehaviour {
 
     float imagewidth;
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         controller = controllerThing.GetComponent<ControllerThing>();
         players = new GameObject[4];
         playerHealth = new Health[4];
@@ -34,7 +36,7 @@ public class UIController : MonoBehaviour {
         {
             players[i] = controller.IDs[i].gameObject;
             playerHealth[i] = players[i].GetComponent<Health>();
-            
+
         }
 
 
@@ -53,17 +55,39 @@ public class UIController : MonoBehaviour {
 
         imagewidth = healthBars[9].rectTransform.rect.width;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         timer -= Time.deltaTime;
         texts[5].text = timer.ToString();
         texts[4].text = bossHealth.health.ToString();
 
-        healthBars[3].material.SetFloat("_Tween", playerCooldowns.activeCooldowns[0]/ playerCooldowns.abilityCooldowns[0]);
+        healthBars[3].material.SetFloat("_Tween", playerCooldowns.activeCooldowns[0] / playerCooldowns.abilityCooldowns[0]);
+        healthBars[4].material.SetFloat("_Tween", playerCooldowns.activeCooldowns[1] / playerCooldowns.abilityCooldowns[1]);
+        healthBars[5].material.SetFloat("_Tween", playerCooldowns.activeCooldowns[2] / playerCooldowns.abilityCooldowns[2]);
 
-        healthBars[25].rectTransform.sizeDelta = new Vector2 (300 * (bossHealth.health / 100), 25);
-       
+        healthBars[9].material.SetFloat("_Tween", playerCooldowns.activeCooldowns[3] / playerCooldowns.abilityCooldowns[3]);
+        healthBars[10].material.SetFloat("_Tween", playerCooldowns.activeCooldowns[4] / playerCooldowns.abilityCooldowns[4]);
+        healthBars[11].material.SetFloat("_Tween", playerCooldowns.activeCooldowns[5] / playerCooldowns.abilityCooldowns[5]);
+
+        healthBars[15].material.SetFloat("_Tween", playerCooldowns.activeCooldowns[6] / playerCooldowns.abilityCooldowns[6]);
+        healthBars[16].material.SetFloat("_Tween", playerCooldowns.activeCooldowns[7] / playerCooldowns.abilityCooldowns[7]);
+        healthBars[17].material.SetFloat("_Tween", playerCooldowns.activeCooldowns[8] / playerCooldowns.abilityCooldowns[8]);
+
+        healthBars[21].material.SetFloat("_Tween", playerCooldowns.activeCooldowns[9] / playerCooldowns.abilityCooldowns[9]);
+        healthBars[22].material.SetFloat("_Tween", playerCooldowns.activeCooldowns[10] / playerCooldowns.abilityCooldowns[10]);
+        healthBars[23].material.SetFloat("_Tween", playerCooldowns.activeCooldowns[11] / playerCooldowns.abilityCooldowns[11]);
+
+
+
+        healthBars[1].rectTransform.sizeDelta = new Vector2(100 * (playerHealth[0].health / 100), 20);
+        healthBars[7].rectTransform.sizeDelta = new Vector2(100 * (playerHealth[1].health / 100), 20);
+        healthBars[13].rectTransform.sizeDelta = new Vector2(100 * (playerHealth[2].health / 100), 20);
+        healthBars[19].rectTransform.sizeDelta = new Vector2(100 * (playerHealth[3].health / 100), 20);
+
+        healthBars[25].rectTransform.sizeDelta = new Vector2(300 * (bossHealth.health / 100), 25);
+
 
 
 
