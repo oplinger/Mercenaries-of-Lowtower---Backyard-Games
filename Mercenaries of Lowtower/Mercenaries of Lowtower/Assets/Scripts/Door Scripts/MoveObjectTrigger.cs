c7c04feff9d8a1ecdf8e7a84a2e2f4f7df85bb8e
SHERNET\WINGEJES <wingejes@sheridancollege.ca>
2017-10-16 12:26:48 -0400

    Create Prefab for Team Mechanic >Add MoveObjectScript

---
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Scripts/Door Scripts/MoveObjectTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObjectTrigger : MonoBehaviour {
+
+    public bool trigger = true;
+    public Vector3 moveDir;
+    public GameObject target;
+
+
+        private void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && trigger == true)
+        {
+           MoveObject(moveDir, target);
+        }
+       
+    }
+        
+    public void MoveObject (Vector3  moveDirection, GameObject target)
+    {
+
+       target.transform.Translate ( moveDirection);
+        trigger = false;
+        //door moves up
+    }
+}
+
