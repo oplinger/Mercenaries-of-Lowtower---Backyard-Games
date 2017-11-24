d863904cb35da001305af9f9bd7ca67f6257b1e6
Benjamin McEachrane <benjamin.mceachrane@gmail.com>
2017-11-24 13:27:41 -0500

    Add "Breaking Barriers" Scenario Scene
    Players can deal damage to rocks on the field, and can destroy
     them. Added new material and texture for breakable objects.

---
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Scripts/BreakableObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableObject : MonoBehaviour {
+
+    Health healthScript;
+    
+    float h1;
+    float h2;
+
+    public Material breakableMat;
+
+    // Use this for initialization
+    void Start () {
+
+        healthScript = GetComponent<Health>();
+        h2 = healthScript.health;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        h1 = healthScript.health;
+
+        if (h1 < h2)
+            {
+                breakableMat.SetColor("_EmissionColor", Color.HSVToRGB(1f, 1, 1));
+                
+                h2 = h1;
+            }
+            else
+            {
+                breakableMat.SetColor("_EmissionColor", Color.HSVToRGB(1f, 1, 0));
+
+            }
+    }
+
+}
