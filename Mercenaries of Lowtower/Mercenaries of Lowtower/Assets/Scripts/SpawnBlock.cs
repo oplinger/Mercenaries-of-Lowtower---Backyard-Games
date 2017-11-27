0fad78f2397e92490a825974e2e53186ce3d972a
Benjamin McEachrane <benjamin.mceachrane@gmail.com>
2017-11-27 13:19:07 -0500

    Add Magnetic Block as Spawnable Obstacle
    "Block Spawn" game object alternates between spawning
    breakable and magnetic blocks.

---
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Scripts/SpawnBlock.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBlock : MonoBehaviour {
+
+    public GameObject blockBreakable;
+    public GameObject blockMagnetic;
+    //public bool rockSpawned;
+    //GameObject currentRock;
+    public int timer;
+
+    //public int randomizeBlock;
+
+    bool magnetic;
+    public bool breakable;
+
+	// Use this for initialization
+	void Start () {
+
+        //breakable = true;
+        //magnetic = false;
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+        //if (!rockSpawned)
+        //{
+        timer += 1;
+
+        if (timer >= 900 && breakable)
+        {
+
+            Instantiate(blockBreakable, transform.position, transform.rotation);
+            breakable = false;
+            //magnetic = true;
+            timer = 0;
+        }
+
+        if (timer >= 900 && !breakable)
+        {
+            Instantiate(blockMagnetic, transform.position, transform.rotation);
+            breakable = true;
+            //magnetic = false;
+            timer = 0;
+        }
+
+    }
+
+
+        }
+       // }
+
+
+        
+	
