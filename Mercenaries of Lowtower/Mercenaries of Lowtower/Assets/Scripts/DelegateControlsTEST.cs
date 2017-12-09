9f7f57e5efa811a9544780da9ca0784117f601ac
Travis Willis <willtrav@sheridancollege.ca>
2017-12-08 21:42:14 -0500

    Delegate Test for Controller Input
    Successful test of delegate implementation

---
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Scripts/DelegateControlsTEST.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelegateControlsTEST : MonoBehaviour {
+    public delegate void ButtonA();
+    public delegate void ButtonAAbility();
+
+    ButtonA buttonA;
+    ButtonAAbility buttonAability;
+
+    public delegate void ButtonB();
+    public delegate void ButtonBAbility(string message);
+
+    ButtonB buttonB;
+    ButtonBAbility buttonBability;
+
+    public PersonThingWithAbilitiesScript personthing;
+    public GameObject personthingwithabilities;
+
+
+    // Use this for initialization
+    void Start () {
+        buttonA = buttonAfunction;
+        buttonB = buttonBfunction;
+        buttonAability = Ability1;
+        buttonBability = Ability2;
+        personthing = personthingwithabilities.GetComponent<PersonThingWithAbilitiesScript>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+		if(Input.GetKey("joystick 1 button 0"))
+        {
+            buttonA();
+        }
+        if (Input.GetKey("joystick 1 button 1"))
+        {
+            buttonB();
+        }
+    }
+
+    public void buttonAfunction()
+    {
+        buttonAability();
+    }
+
+    public void buttonBfunction()
+    {
+        buttonBability("horse");
+    }
+
+    public void Ability1()
+    {
+        print("lokoipj");
+    }
+
+    public void Ability2(string message)
+    {
+        buttonAability = Ability3;
+        print(message);
+    }
+    public void Ability3()
+    {
+        print("Ability3 Fired!");
+    }
+}
---
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Scripts/PersonThingWithAbilitiesScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonThingWithAbilitiesScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PersonThingAbility1()
+    {
+        print("I am a normal Person Thing. I do Things.");
+    }
+}
