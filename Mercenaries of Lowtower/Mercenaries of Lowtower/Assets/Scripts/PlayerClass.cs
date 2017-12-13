6a6cd053285ca3c2b480a4e6e210add514cc365e
oplinger-PC\oplinger <willtrav@sheridancollege.ca>
2017-12-13 09:37:42 -0500

    Add player, Melee, and Tank classes. Rework jumping code to function with inheritance
    Put all shared values in a player class, and created character classes that inherit these values. Used an inherited jump function. Jump works exactly the same, but is now only created/called once.

---
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Scripts/PlayerClass.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerClass : MonoBehaviour 
+{
+    public int CTRLID;
+    public GameObject controllerThing;
+     public Vector3 playermovement;
+    public ControllerThing controller;
+    public PlayerAbilityController abilities;
+    public PlayerCDController cooldowns;
+    public Collider[] colliders;
+    public Animator anim;
+    public Health health;
+    public float currentHealth;
+    public float lastFrameHealth;
+
+    public float walkspeed;
+    public float jumpHeight;
+    public LayerMask groundMask;
+    public LayerMask magnetMask;
+    public Material playerMat;
+    public float reviveRadius;
+    public float reviveCastTime;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+    public int JumpCheck()
+    {
+        Debug.DrawRay(transform.position, Vector3.up * -1f);
+        colliders = Physics.OverlapCapsule(transform.position, transform.position + (Vector3.up * -2f), .25f, groundMask, QueryTriggerInteraction.Ignore);
+       // print(colliders[0]);
+        int length;
+        length = colliders.Length;
+        return length;
+    }
+
+    public void Jump(float jumpheight)
+    {
+        GetComponent<Rigidbody>().AddForce(0, jumpheight, 0, ForceMode.Impulse);
+    }
+
+    public void Movement()
+    {
+
+    }
+    
+    public void Revive()
+    {
+
+    }
+
+    public void Death()
+    {
+
+    }
+
+
+}
---
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Scripts/TankClass.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class TankClass : PlayerClass
+{
+    [SerializeField]
+    int grounded;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        grounded = JumpCheck();
+
+        if (grounded > 0)
+        {
+
+            Jump(5);
+        }
+        else
+        {
+            Jump(0);
+        }
+	}
+}
