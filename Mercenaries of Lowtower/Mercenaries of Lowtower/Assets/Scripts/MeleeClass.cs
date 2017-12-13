e2cc0659111248c6cc59c3abdbc5cb017754023b
oplinger-PC\oplinger <willtrav@sheridancollege.ca>
2017-12-13 12:05:57 -0500

    Interface Testing
    Add interface inheritance to the melee class for jumping (IJumpable) both work exactly the same now.

--- Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Scripts/MeleeClass.cs
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Scripts/MeleeClass.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MeleeClass : PlayerClass {
-
-	// Use this for initialization
-	void Start () {
+public class MeleeClass : MonoBehaviour, IJumpable<float> {
+    public int grounded;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        //grounded = JumpCheck();
 
-	}
+        if (grounded > 0)
+        {
+            Jump(5);
+        }
+    }
+
+    public void Jump(float jumpHeight)
+    {
+        GetComponent<Rigidbody>().AddForce(0, jumpHeight, 0, ForceMode.Impulse);
+
+    }
+
 }
