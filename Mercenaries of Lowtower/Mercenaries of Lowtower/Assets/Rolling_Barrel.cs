7a243091a590840af11ab196439fb957ffe0ca64
WING <wingejes@sheridancollege.ca>
2018-02-13 16:00:33 -0500

    I added Standard_Level_with_Barrels to prefabs I created a - Barrel_Manager script                     - Rolling Barrel script I added a Training_Level_Template to prefabs If I hadn't done that already I added the barrel.fbx.

---
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Rolling_Barrel.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rolling_Barrel : MonoBehaviour
+{
+
+    // Original transform position
+    Vector3 originPoint;
+    // Transform for the waypoint of how far the beam will move
+    public Transform barrelEndPoint;
+    // The speed at which the tentacle moves toward the waypoint
+    public float barrelSpeed;
+    // bool to check if the endPoint has been reached
+    public bool endPointReached;
+
+    // Holds a reference to the BossManager game object
+    public GameObject BarrelManager;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Sets the origin point to the position the tentacles are in when the play button is pressed
+        originPoint = gameObject.transform.position;
+      //  barrelSpeed = BarrelManager.GetComponent<Barrel_Manager>().barrelSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (endPointReached == false)
+        {
+            MoveBarrel();
+        }
+        if (endPointReached == true)
+        {
+            ReturnBarrel();
+        }
+    }
+
+    void MoveBarrel()
+    {
+        // Multiplies the elevation speed by time
+        float step = barrelSpeed * Time.deltaTime;
+        // Moves the arm up towards the endPoint at a set speed
+        transform.position = Vector3.MoveTowards(transform.position, barrelEndPoint.transform.position, step);
+
+        if (transform.position == barrelEndPoint.transform.position)
+        {
+            endPointReached = true;
+        }
+    }
+
+    void ReturnBarrel()
+    {
+        // Multiplies the elevation speed by time
+        float step = barrelSpeed * Time.deltaTime;
+        // Moves the arm up towards the endPoint at a set speed
+        transform.position = Vector3.MoveTowards(transform.position, originPoint, step);
+
+        if (transform.position == originPoint)
+        {
+            endPointReached = false;
+        }
+    }
+}
