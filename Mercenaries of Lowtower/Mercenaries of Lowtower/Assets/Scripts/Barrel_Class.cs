c5210dee1326271c836f6f692a631251fa000642
WING <wingejes@sheridancollege.ca>
2018-02-16 12:37:27 -0500

    Updated the Barrel_Class so the waypoints are now can be assigned to the barrel rather than having a seperate Rolling_Barrel script.

--- Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Scripts/Barrel_Class.cs
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Scripts/Barrel_Class.cs
@@ -8,17 +8,33 @@ public class Barrel_Class : EnemyClass {
     public Material breakableMat;
     Renderer rend;
     public bool breakable;
+    public bool rolling;
+    //public Transform barrelEndPoint;
+    public float barrelSpeed;
+    public Transform[] barrelWaypoints;
+    public bool endPointReached;
+    public Transform target;
+
 
     // Use this for initialization
     void Start () {
+        //barrelWaypoints = new Transform[2];
+       
         currentHealth = maxHealth;
         rend = GetComponent<Renderer>();
         breakableMat = rend.material;
         lastFrameHealth = currentHealth;
+        target = barrelWaypoints[0];
+
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
+        if (rolling)
+        {
+            MoveBarrel();
+        }
+
         if (breakable)
         {
             if (currentHealth < lastFrameHealth)
@@ -42,4 +58,36 @@ public class Barrel_Class : EnemyClass {
 
         }
     }
+
+    void MoveBarrel()
+    {
+        // Multiplies the elevation speed by time
+        float step = barrelSpeed * Time.deltaTime;
+        
+        // Moves the arm up towards the endPoint at a set speed
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+
+        if (transform.position == barrelWaypoints[1].transform.position)
+        {
+            target = barrelWaypoints[0];
+        }
+
+        if (transform.position == barrelWaypoints[0].transform.position)
+        {
+            target = barrelWaypoints[1];
+        }
+    }
+
+    //void ReturnBarrel()
+    //{
+    //    // Multiplies the elevation speed by time
+    //    float step = barrelSpeed * Time.deltaTime;
+    //    // Moves the arm up towards the endPoint at a set speed
+    //    transform.position = Vector3.MoveTowards(transform.position, barrelWaypoints[0].position, step);
+
+    //    if (transform.position == barrelWaypoints[0].position)
+    //    {
+    //        endPointReached = false;
+    //    }
+    //}
 }
