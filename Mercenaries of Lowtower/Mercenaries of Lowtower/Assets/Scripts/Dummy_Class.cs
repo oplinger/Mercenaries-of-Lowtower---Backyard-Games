872632f5fee09045e48b5761ce48ff017123c7fb
WING <wingejes@sheridancollege.ca>
2018-02-23 17:41:12 -0500

    Created Tutorial Level, and Dummy_Class Script

---
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Scripts/Dummy_Class.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dummy_Class : PlayerClass {
+
+	// Use this for initialization
+	void Start () {
+        maxHealth = 100;
+        currentHealth = 50;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
