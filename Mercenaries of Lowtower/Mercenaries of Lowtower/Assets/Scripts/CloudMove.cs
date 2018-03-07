1573a14e4b7c9f515b369e3f62245c22855f4438
Travis Willis <willtrav@sheridancollege.ca>
2018-03-06 19:31:24 -0500

    Add script to scroll the cloud layer

---
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Scripts/CloudMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMove : MonoBehaviour {
+    Renderer rend;
+    public float Xmove;
+    public float Ymove;
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rend.material.mainTextureOffset += new Vector2(Xmove, Ymove);
+	}
+}
