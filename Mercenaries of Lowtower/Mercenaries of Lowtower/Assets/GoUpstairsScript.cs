5b7c7ec1b7321370c4776c13657a7e1a27cb551c
WING <wingejes@sheridancollege.ca>
2018-03-08 15:23:36 -0500

    Added and edited the some Font fot he Starting UI Changed the Background Colour Added Starting in 3,2,1 text to lauch the game.

--- Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/GoUpstairsScript.cs
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/GoUpstairsScript.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -9,41 +11,75 @@ public class GoUpstairsScript : MonoBehaviour {
     public int playerCount;
     public Text text;
     public int playersReady;
-
+    private float timeRemaining = 10;
+    public bool timer = false;
+    public int counter = 0;
+ 
 	// Use this for initialization
 	void Start () {
         playerCount = 0;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        playersReady = 4 - playerCount;
-        print(playersReady);
+                       
 	}
 
+   
+	// Update is called once per frame
+	void Update () {
+        //timeRemaining -= Time.deltaTime;
+        playersReady = 4 - playerCount;
+        print(playersReady);
+        if (playerCount == 0)
+        {
+            text.text = "ALL 4 PLAYERS PLEASE STAND" + Environment.NewLine + "ON THE STAIRS TO START";
+        }
+      }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag=="Player")
         {
             playerCount += 1;
-            if (playerCount == 4)
+            /*
+            if (playerCount == 1 && timeRemaining > 0) //playerCount ==4
             {
-                SceneManager.LoadScene(1);
+              text.text = "Starting in : " +(int)timeRemaining;
+                if (timeRemaining <= 0)
+                {
+                    SceneManager.LoadScene(1);
+                    }
+                 }
+               }
             }
+            */
         }
     }
 
     private void OnTriggerStay(Collider other)
     {
         //print("something has entered" + playerCount + "more players needed");
-        text.text = playersReady + " more players needed to start the game.";
+        //text.text = playersReady + " MORE PLAYERS NEEDED" + Environment.NewLine + "TO START THE GAME.";
+
+        if(playerCount == 4) //playerCount == 4
+        {
+            timeRemaining -= Time.deltaTime;
+            text.text = "STARTING IN  " + (int)timeRemaining/2;
+            if (timeRemaining <= 0)
+            {
+                SceneManager.LoadScene(1);
+            }
+        }
+        else
+        {
+            text.text = playersReady + " MORE PLAYERS NEEDED" + Environment.NewLine + "TO START THE GAME.";
+        }
     }
     private void OnTriggerExit(Collider other)
     {
-        text.text = playersReady + " more players needed to start the game.";
+        text.text = playersReady + " MORE PLAYERS NEEDED" + Environment.NewLine + "TO START THE GAME.";
         if (other.tag=="Player")
         {
+            timeRemaining = 50;
             playerCount -= 1;
+
+           
         }
     }
 }
