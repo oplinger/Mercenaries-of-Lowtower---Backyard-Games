e7789fd0be009bd20ddd609dc156dccd01715833
Benjamin <benjamin.mceachrane@gmail.com>
2018-03-07 23:18:11 -0500

    EGLX Week Update - Win / Lose UI, Pause Screen
    Game can be paused and un-paused by
    any player. Game displays victory screen
    if players win, and displays lose screen if
    all players die. Started to implement scene
    loading, but errors (presumably) caused by
    healing orb prevent main gameplay scene from
    reloading correctly.

---
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Scripts/LoadingScreenBoss.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class LoadingScreenBoss : MonoBehaviour {
+
+    public GameObject logo;
+    int counter;
+
+	// Use this for initialization
+	void Start () {
+        counter = 0;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        counter++;
+
+        float scale = 2 + Mathf.PingPong(Time.time/1.5f, 1);
+        logo.transform.localScale = new Vector3(scale, scale, scale);
+
+        if (counter >=120)
+        {
+            //print("scene done loading");
+            SceneManager.LoadScene(1);
+        }
+
+    }
+}
--- Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Scripts/MenuUIController.cs
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Scripts/MenuUIController.cs
@@ -2,19 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuUIController : MonoBehaviour {
 
     public GameObject winScreen;
+    public GameObject restartButton;
+    public GameObject gamePaused;
+    public GameObject startToResume;
+
+    public GameObject loseScreen;
+    public GameObject startToRetry;
+
     public GameObject bossManager;
     public BossControlScript bossManagerScript;
+
+    public bool gameIsPaused;
+    public bool pauseDisabled;
+
+    public TankClass tankScript;
+    public HealerClass healerScript;
+    public MeleeClass meleeScript;
+    public RangedClass rangedScript;
 
     // Use this for initialization
     void Start () {
 
-        winScreen.SetActive(false);
+        //winScreen.SetActive(false);
 
         bossManagerScript = bossManager.GetComponent<BossControlScript>();
+        gameIsPaused = false;
+
+        gamePaused.SetActive(false);
+        startToResume.SetActive(false);
+        
+        loseScreen.SetActive(false);
+        startToRetry.SetActive(false);
+
+        pauseDisabled = false;
+
+        //tankScript = GameObject.Find("Tank Character(Clone)").GetComponent<TankClass>();
+        //healerScript = GameObject.Find("Healer Character(Clone)").GetComponent<HealerClass>();
+        //meleeScript = GameObject.Find("Melee Character(Clone)").GetComponent<MeleeClass>();
+        //rangedScript = GameObject.Find("Ranged Character(Clone)").GetComponent<RangedClass>();
 
 
     }
@@ -22,10 +52,72 @@ public class MenuUIController : MonoBehaviour {
     // Update is called once per frame
     void Update () {
 
+        //PAUSE GAME
+        if (Input.GetKeyDown("joystick button 7") && !pauseDisabled)
+        {
+
+            if (!gameIsPaused)
+            {
+                print("game paused");
+                gameIsPaused = true;
+                Time.timeScale = 0;
+                gamePaused.SetActive(true);
+                startToResume.SetActive(true);
+            }
+
+            else
+            {
+                print("game unpaused");
+                gameIsPaused = false;
+                Time.timeScale = 1;
+                gamePaused.SetActive(false);
+                startToResume.SetActive(false);
+            }
+
+            
+        }
+
+        //restarts boss scene if Back button is pressed while pause menu is up
+        if (gameIsPaused && Input.GetKeyDown("joystick button 6"))
+        {
+
+            //print("restart scene");
+            //SceneManager.LoadScene(2);
+        }
+
+
+
+
+
+        //WIN THE GAME 
 
         if (bossManagerScript.bossPhase == 8)
         {
             winScreen.SetActive(true);
+            restartButton.SetActive(true);
+            float scale = .22f + Mathf.PingPong(Time.time/18, .15f);
+            winScreen.transform.localScale = new Vector3(scale,scale,scale);
+
+            pauseDisabled = true;
+
+            if ((Input.GetKeyDown("joystick button 7")))
+            {
+                print("game restarted");
+            }
+
+       
+        }
+
+        //LOSE THE GAME
+
+        if (tankScript.currentHealth<=0 && healerScript.currentHealth<=0 && meleeScript.currentHealth<=0 && rangedScript.currentHealth<=0)
+        {
+            loseScreen.SetActive(true);
+            startToRetry.SetActive(true);
+            float scale = .22f + Mathf.PingPong(Time.time / 18, .15f);
+            loseScreen.transform.localScale = new Vector3(scale, scale, scale);
+
+            pauseDisabled = true;
 
             if ((Input.GetKeyDown("joystick button 7")))
             {
