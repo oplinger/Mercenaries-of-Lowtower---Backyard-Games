e566e9b821e8fa971a687f14b97870b01ef4657d
WING <wingejes@sheridancollege.ca>
2018-03-08 00:07:29 -0500

    > Created Character Prefabs > Added UI changes to detected players on the stairs.

--- Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/GoUpstairsScript.cs
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/GoUpstairsScript.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GoUpstairsScript : MonoBehaviour {
 
     public int playerCount;
+    public Text text;
+    public int playersReady;
 
 	// Use this for initialization
 	void Start () {
@@ -14,12 +17,12 @@ public class GoUpstairsScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        playersReady = 4 - playerCount;
+        print(playersReady);
 	}
 
     private void OnTriggerEnter(Collider other)
     {
-        print("something has entered");
         if (other.tag=="Player")
         {
             playerCount += 1;
@@ -30,8 +33,14 @@ public class GoUpstairsScript : MonoBehaviour {
         }
     }
 
+    private void OnTriggerStay(Collider other)
+    {
+        //print("something has entered" + playerCount + "more players needed");
+        text.text = playersReady + " more players needed to start the game.";
+    }
     private void OnTriggerExit(Collider other)
     {
+        text.text = playersReady + " more players needed to start the game.";
         if (other.tag=="Player")
         {
             playerCount -= 1;
