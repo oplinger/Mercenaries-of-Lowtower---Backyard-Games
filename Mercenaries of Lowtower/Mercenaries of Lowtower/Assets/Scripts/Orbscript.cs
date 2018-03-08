858c1de0b92114435826ce0a4a969042d5c613c9
Travis Willis <willtrav@sheridancollege.ca>
2018-03-07 20:04:08 -0500

    Healer orb now tracks closest target within a set number of units

--- Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Scripts/Orbscript.cs
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Scripts/Orbscript.cs
@@ -7,21 +7,45 @@ public class Orbscript : MonoBehaviour {
     GameObject Target;
     float healAmount;
     float orbSpeed;
+    public float trackingDistance;
     int ID;
+    Collider[] cols;
+    public LayerMask PlayerMask;
+    float closestDistance;    
+    float[] distances;
+
 	// Use this for initialization
 	void Start () {
         //IDamageable damage = (IDamageable)GetComponent(typeof(IDamageable));
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        Physics.OverlapSphereNonAlloc(transform.position, trackingDistance, cols, PlayerMask, QueryTriggerInteraction.Ignore);
+        if (cols != null)
+        {
+            for (int i = 0; i < cols.Length; i++)
+            {
+                closestDistance = 100f;
+                distances[i] = (Vector3.Distance(transform.position, cols[i].transform.position));
+                if (distances[i] < closestDistance)
+                {
+                    Target = cols[i].gameObject;
+                }
+
+            }
+        }
+
+
         transform.position = Vector3.MoveTowards(transform.position, Target.transform.position, orbSpeed*Time.deltaTime);
 	}
 
     public void OrbBehavior(GameObject origin, GameObject target, float healamount, float orbspeed, int playerID)
     {
          Origin=origin;
-         Target=target;
+         //Target=target;
          healAmount=healamount;
          orbSpeed=orbspeed;
          playerID=ID;
