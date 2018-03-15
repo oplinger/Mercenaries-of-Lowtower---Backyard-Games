4634f0bcd3ed7c1e98f893e420b7ef910bcb7b6d
WING <wingejes@sheridancollege.ca>
2018-03-15 14:26:04 -0400

    Changing the Tutorial Level, to hopefully be more accomdating for players. > Added Training Room_Target Script so when the ranger shoots the target it turns red > Added Enemy Pen and Enemy Spawner prefabs

---
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/TrainingRoom_Target.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainingRoom_Target : MonoBehaviour {
+
+    Renderer targetRenderer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Bolt")
+        {
+            targetRenderer.material.SetColor("_EmissionColor", Color.red);
+        }
+        else
+        {
+            targetRenderer.material.SetColor("_EmissionColor", Color.black);
+        }
+         
+        }
+        
+    }
+
