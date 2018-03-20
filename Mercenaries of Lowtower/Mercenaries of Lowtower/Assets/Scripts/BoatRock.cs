f8b50ecaccaf00bdf8f7873f7bc1cacc19793ea7
Travis Willis <willtrav@sheridancollege.ca>
2018-03-20 12:28:05 -0400

    Add boat rock and camera shake

---
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Scripts/BoatRock.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatRock : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += new Vector3(Mathf.Sin(Time.time*.125f)*.001f, Mathf.Sin(Time.time * .062f) * .00025f, Mathf.Sin(Time.time *.25f) * .001f);
+        transform.rotation *= Quaternion.Euler(new Vector3(Mathf.Sin(Time.time * .125f) * .001f, Mathf.Sin(Time.time * .062f) * .00025f, Mathf.Sin(Time.time * .25f) * .001f));
+	}
+}
---
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour {
+    public Vector3 defaultPos;
+    public float intensity;
+    public float duration;
+    public float countdown;
+
+    public bool shaking;
+	// Use this for initialization
+	void Start () {
+        defaultPos = transform.position;
+        countdown = duration;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (shaking)
+        {
+            if (countdown > 0)
+            {
+                countdown -= Time.deltaTime;
+                transform.position = new Vector3(Random.Range(defaultPos.x - (1 * intensity), defaultPos.x + (1 * intensity)), Random.Range(defaultPos.y - (1 * intensity), defaultPos.y + (1 * intensity)), defaultPos.z/*Random.Range(defaultPos.z - (1*intensity), defaultPos.z + (1*intensity))*/);
+            }
+            else
+            {
+                transform.position = defaultPos;
+                shaking = false;
+                countdown = duration;
+            }
+        }
+	}
+}
