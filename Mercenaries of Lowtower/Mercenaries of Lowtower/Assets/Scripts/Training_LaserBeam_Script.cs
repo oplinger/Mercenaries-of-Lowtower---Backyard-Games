9bf5eee25a1e7de557e7873282df110ca1d533b9
WING <wingejes@sheridancollege.ca>
2018-03-27 13:21:23 -0400

    - Updated the Tutorial Scene             >Added damage script to the training laser             >Added cannonballs (can't be picked up for some reason)             >Added new minions to the spawner - Added different Stun Texture to the Healer

---
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Scripts/Training_LaserBeam_Script.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Training_LaserBeam_Script : MonoBehaviour {
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player") 
+        {
+            other.GetComponent<IDamageable<float>>().TakeDamage(40);
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
