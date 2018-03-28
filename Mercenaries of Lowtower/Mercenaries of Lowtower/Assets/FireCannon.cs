683b314241e468b45a4fb33c6e71548290e7ba87
Benjamin <benjamin.mceachrane@gmail.com>
2018-03-28 19:18:53 -0400

    "Animate" Cannon
    Cannon flashes when loaded with
    cannonball, then squashes & stretches.
    Cannonball shot not yet linked to play after
    animation. Lighten boss texture slightly.

--- Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/FireCannon.cs
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/FireCannon.cs
@@ -10,16 +10,31 @@ public class FireCannon : MonoBehaviour {
     public GameObject bossManager;
     public Transform cannonballSpawner;
 
+    public Renderer cannonRenderer;
+    public Color flashColour;
+    public Color flashColour2;
+    public int flashTime;
+
+    public GameObject cannonBarrel;
+
+    public float ogScale;
     //Reference to the phase-changing script
     //public PhaseControllerScript phaseScript;
 
     // Use this for initialization
     void Start () {
+
+        cannonRenderer = cannonBarrel.GetComponent<Renderer>();
+
+        ogScale = cannonBarrel.transform.localScale.y;
+        
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+
 
 
     }
@@ -32,6 +47,7 @@ public class FireCannon : MonoBehaviour {
         {
 
             //phaseScript.cannonFired = true;
+                StartCoroutine(PumpColour(flashColour, flashTime));
             bossManager.GetComponent<BossControlScriptV2>().cannonFired = true;
             Destroy(other.gameObject);
             Instantiate(cannonballProp, new Vector3(transform.position.x, transform.position.y+3, transform.position.z), transform.rotation);
@@ -52,5 +68,58 @@ public class FireCannon : MonoBehaviour {
             //Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
             //rb.AddForce(transform.forward * 2);
         }
+    }
+
+
+    private IEnumerator PumpColour(Color target, float time)
+    {
+
+        //float timer = 0;
+        //while (timer < time)
+        //{
+        //    timer += Time.deltaTime;
+        //    cannonRenderer.material.SetColor("_EmissionColor", Color.Lerp(flashColour, flashColour2, Mathf.PingPong(Time.time * 3, 1)));
+        //    yield return null;
+        //}
+
+       float timer = 0;
+        while (timer < time)
+        {
+            timer += Time.deltaTime;
+            cannonRenderer.material.SetColor("_EmissionColor", Color.Lerp(flashColour, flashColour2, Mathf.PingPong(Time.time * 9, 1)));
+            yield return null;
+        }
+
+        //cannon squash
+         timer = 0;
+        time = .3f;
+        while (timer < time)
+        {
+            timer += Time.deltaTime;
+            cannonRenderer.material.SetColor("_EmissionColor", Color.black);
+            cannonBarrel.transform.localScale -= new Vector3(0, .05f, 0);
+            yield return null;
+        }
+
+        //cannon stretch
+         timer = 0;
+        while (timer < time)
+        {
+            timer += Time.deltaTime;
+            cannonRenderer.material.SetColor("_EmissionColor", Color.Lerp(flashColour, flashColour2, Mathf.PingPong(Time.time * 9, 2)));
+            cannonBarrel.transform.localScale += new Vector3(0, .5f, 0);
+            yield return null;
+        }
+
+        //cannon return
+        timer = 0;
+        while (timer < time)
+        {
+            timer += Time.deltaTime;
+            cannonRenderer.material.SetColor("_EmissionColor", Color.black);
+            cannonBarrel.transform.localScale = Vector3.Lerp(cannonBarrel.transform.localScale, new Vector3(ogScale, ogScale, ogScale), time);
+            yield return null;
+        }
+
     }
 }
