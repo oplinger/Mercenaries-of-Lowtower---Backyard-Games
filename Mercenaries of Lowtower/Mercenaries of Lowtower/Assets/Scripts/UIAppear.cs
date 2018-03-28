4921a54a05bcdb9282eabb31bec37bde32a530a7
WING <wingejes@sheridancollege.ca>
2018-03-28 15:59:01 -0400

    Small changes to the UI in the tutorial level.

---
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Scripts/UIAppear.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIAppear : MonoBehaviour
+{
+    [SerializeField] private Text customText;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            customText.enabled = true;
+        }
+    }
+
+
+
+        void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                customText.enabled = false;
+            }
+        }
+}
