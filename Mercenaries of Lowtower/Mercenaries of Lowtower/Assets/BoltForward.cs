07176b780ab4a4eec4c85b592e0a2ff018c89801
MARIO <marmario@sheridancollege.ca>
2018-04-03 16:04:50 -0400

    Add bolt effect to ranged character

--- Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/BoltForward.cs
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/BoltForward.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BoltForward : MonoBehaviour {
 
+    public float boltSpeed;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,7 +13,14 @@ public class BoltForward : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(Vector3.forward);
+        if (gameObject.name == "BluntArrow(Clone)")
+        {
+            transform.Translate(Vector3.forward * boltSpeed);
+        }
+        else
+        {
+            transform.Translate(Vector3.forward);
+        }
         //Destroy(gameObject, 5);
 	}
 }
