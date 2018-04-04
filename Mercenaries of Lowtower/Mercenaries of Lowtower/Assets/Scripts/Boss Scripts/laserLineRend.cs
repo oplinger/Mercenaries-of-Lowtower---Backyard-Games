623346fb59a7d7acd149b59db432fa3a29352f07
MARIO <marmario@sheridancollege.ca>
2018-04-03 22:30:09 -0400

    Add tentacle lasers

---
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Scripts/Boss Scripts/laserLineRend.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laserLineRend : MonoBehaviour {
+
+    public Transform laserStart;
+    public Transform laserEnd;
+    LineRenderer laserLine;
+
+	// Use this for initialization
+	void Start () {
+        laserLine = GetComponent<LineRenderer>();
+        laserLine.startWidth = 2f;
+        laserLine.endWidth = 2f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        laserLine.SetPosition(0, laserStart.position);
+        laserLine.SetPosition(1, laserEnd.position);
+    }
+}
