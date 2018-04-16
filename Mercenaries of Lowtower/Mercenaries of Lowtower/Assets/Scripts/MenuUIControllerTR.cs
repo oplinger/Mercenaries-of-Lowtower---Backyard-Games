e6df20b36cff74625e63229486560860c35bbfde
Benjamin <benjamin.mceachrane@gmail.com>
2018-04-16 12:02:19 -0400

    Add Controls to Pause Menu, Adjust Menu Prefabs
    Put player controls on the pause menu. Add
    black semi-transparent background to make
    menu items pop visually. Create new prefab
    for training room's pause menu.

---
+++ Mercenaries of Lowtower/Mercenaries of Lowtower/Assets/Scripts/MenuUIControllerTR.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuUIControllerTR : MonoBehaviour {
+    
+    public GameObject gamePaused;
+    public GameObject startToResume;
+    
+    public bool gameIsPaused;
+    public bool pauseDisabled;
+
+    // Use this for initialization
+    void Start () {
+        
+        
+        gameIsPaused = false;
+
+        gamePaused.SetActive(false);
+        startToResume.SetActive(false);
+
+        pauseDisabled = false;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        //CHEAT TO RETURN TO LOBBY
+        if (gameIsPaused && Input.GetKey("joystick button 4") && Input.GetKey("joystick button 5") && Input.GetKey("joystick button 0"))
+        {
+
+            Time.timeScale = 1;
+            SceneManager.LoadScene(1);
+        }
+
+        //PAUSE GAME
+        if (Input.GetKeyDown("joystick button 7") && !pauseDisabled)
+        {
+
+            if (!gameIsPaused)
+            {
+                print("game paused");
+                gameIsPaused = true;
+                Time.timeScale = 0;
+                gamePaused.SetActive(true);
+                startToResume.SetActive(true);
+            }
+
+            else
+            {
+                print("game unpaused");
+                gameIsPaused = false;
+                Time.timeScale = 1;
+                gamePaused.SetActive(false);
+                startToResume.SetActive(false);
+            }
+
+            
+        }
+
+    }
+}
